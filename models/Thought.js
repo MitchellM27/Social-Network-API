d72137a0a931e63310f62ab3d5dceb1973a44211
Mitchell <mitchellm27@gmail.com>
2022-04-19 20:04:29 -0700

    adjusted thought.js and added thought model.

---
+++ models/Thought.js
@@ -0,0 +1,37 @@
+const {Schema, model} = require('mongoose');
+  const moment = require('moment');
+  const reactionSchema = require('./Reaction');
+  
+  const ThoughtSchema = new Schema(
+    {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdTime => moment(createdTime).format('MM DD, YYYY [at] hh:mm a')
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [reactionSchema]
+  }, {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  });
+  
+  ThoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+  });
+  
+  const Thought = model('Thought', ThoughtSchema);
+  
+  module.exports = Thought;
