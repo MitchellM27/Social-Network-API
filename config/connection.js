a7ebd5e004e7e651d33aabf45dc481eba2b77699
Mitchell <mitchellm27@gmail.com>
2022-04-19 21:08:46 -0700

    added get all thoughts route.

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,6 @@ const { connect, connection } = require('mongoose');
 connect('mongodb://localhost/socialNetworkAPI', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+}); 
 
 module.exports = connection;
