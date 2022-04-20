d14c36dba27d03008b25d6229cbcbbedfd4f39de
Mitchell <mitchellm27@gmail.com>
2022-04-19 19:57:08 -0700

    added server files and config connection.

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const routes = require ('./routes')
+const db = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(routes);
+
+db.once('open', () => {
+    app.listen(PORT, () => console.log(`connected to localhost:${PORT}`));
+  });
