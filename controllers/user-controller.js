97d75d321a264f5b2c38ee48ae8edfd670655ea1
Mitchell <mitchellm27@gmail.com>
2022-04-19 20:16:22 -0700

    added create user and get user by ID.

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,8 +1,33 @@
-const {
-    User
-} = require('../models');
+const {User} = require('../models');
 
 const userController = {
+    //creating a user
+    createUser({
+        body
+    }, res) {
+        User.create(body)
+            .then(userData => res.json(userData))
+            .catch(err => res.status(400).json(err));
+    },
+
+    //getting a user by their ID
+    getUserById({
+        params
+    }, res) {
+        User.findOne({
+                _id: params.id
+            })
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(userData => res.json(userData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
 
     
 };
